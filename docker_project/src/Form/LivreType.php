98eb1698b2ad7c980b0101d376cd1e25ff789358
l3miage-larbiy <larbi1992yasmine@gmail.com>
2024-02-08 15:07:04 +0100

    controller_livre

---
+++ docker_project/src/Form/LivreType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('nom', TextType::class, [
+            'label' => 'Nom',
+            'required' => true,
+            'attr' => [
+                'placeholder' => 'Nom du livre'
+            ]
+        ])
+        ->add('autheur', TextType::class, [
+            'label' => 'Auteur',
+            'required' => true,
+        ])
+        ->add('nombreDePage', TextType::class, [
+            'label' => 'Nombre de page',
+            'required' => true,
+            'attr' => [
+                'placeholder' => 'Nombre de page'
+            ]
+            
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
